582ee08a1b9dd733c5db2800e21220c3b17739e7
xiaolengjing <101035751+xiaolengjing@users.noreply.github.com>
2022-03-06 14:48:45 +0800

    Add files via upload

---
+++ js/autoload.js
@@ -0,0 +1,14 @@
+try {
+    $("<link>").attr({href: "https://imgs.sakikency.top/assets/waifu.css", rel: "stylesheet", type: "text/css"}).appendTo('head');
+    $('body').append(
+        '<div class="waifu"><div class="waifu-tips"></div><canvas id="live2d" class="live2d"></canvas><div class="waifu-tool"><span class="fui-home"></span> <span class="fui-chat"></span> <span class="fui-eye"></span> <span class="fui-user"></span> <span class="fui-photo"></span> <span class="fui-info-circle"></span> <span class="fui-cross"></span></div></div>');
+    $.ajax({url: 'https://imgs.sakikency.top/assets/waifu-tips.min.js',dataType:"script", cache: true, async: false});
+    $.ajax({url: 'https://imgs.sakikency.top/assets/live2d.min.js',dataType:"script", cache: true, async: false});
+    /* 可直接修改部分参数 */
+    live2d_settings['hitokotoAPI'] = 'hitokoto.cn';  // 一言 API
+    live2d_settings['modelId'] = 6;                  // 默认模型 ID    6/6.7.8.9
+    live2d_settings['modelTexturesId'] = 9;          // 默认材质 ID
+    live2d_settings['modelStorage'] = false;         // 不储存模型 ID
+    /* 在 initModel 前添加 */
+    initModel('https://imgs.sakikency.top/assets/waifu-tips.json');
+} catch(err) { console.log('[Error] JQuery is not defined.') }
